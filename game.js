963067508f2bd3186723d6d933f37e185c18581b
Кирилл Бархатов <117072370+KIrill221Barhatov@users.noreply.github.com>
2023-03-06 17:09:11 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,17 @@
+var character = document.getElementById("character");
+
+document.addEventListener("click", function(event) {
+    jump ();
+})
+ 
+function jump () {
+    if (character.classList != 'animate') {
+        character.classList.add('animate')
+    }
+    setTimeout(function(){
+        character.classList.remove('animate')
+    }, 300)
+} 
+
+
+
